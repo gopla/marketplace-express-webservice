2bb3986b8e4936ba9532f992b444fc80c0383bb1
Naufal Yudhistira <yudhistn@gmail.com>
2019-06-15 14:33:37 +0700

    Add Keranjang Service

--- controller/keranjangController.js
+++ controller/keranjangController.js
@@ -1,14 +1,32 @@
 const Keranjang = require("../model/Keranjang");
+const Produk = require("../model/Produk")
 
 module.exports = {
   index(req, res) {
-    Keranjang.findAll().then(function(rows) {
+    Keranjang.findAll({
+        include: {
+            model: Produk
+        }
+    }).then(function(rows) {
       res.json(rows);
     });
   },
   store(req, res) {
-    Keranjang.create(req.body).then(function(row) {
-      res.json(row);
+    Keranjang.findAll({
+      where: {
+        id_produk: req.body.id_produk
+      }
+    }).then(function(rows) {
+      if (rows.length) {
+        Keranjang.findByPk(rows[0].id_keranjang).then(function(row) {
+          row.update(req.body);
+          res.json(row);
+        });
+      } else {
+        Keranjang.create(req.body).then(function(row) {
+          res.json(row);
+        });
+      }
     });
   },
   update(req, res) {
