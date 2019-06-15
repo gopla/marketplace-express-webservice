80328499b3788b060eb52b408e5c88090141aa29
Naufal Yudhistira <yudhistn@gmail.com>
2019-06-15 12:55:48 +0700

    Add Routes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 app.use("/produk", require("./routes/produkRouter"));
 app.use("/user", require("./routes/userRouter"));
-app.use("/keranjang");
+app.use("/keranjang", require("./routes/keranjangRouter"));
 
 app.listen(process.env.PORT || 3000, function() {
   console.log(" -> Server listening on port 3000");
