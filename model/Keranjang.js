854b6e64a2ebe80287b3d061302c2871c7b58736
Naufal Yudhistira <yudhistn@gmail.com>
2019-06-15 17:59:33 +0700

    Fixed Keranjang model

--- model/Keranjang.js
+++ model/Keranjang.js
@@ -1,10 +1,6 @@
 const Sequelize = require("sequelize");
 const sequelize = require("../config/db");
-<<<<<<< HEAD
-const DetailKeranjang = require("./DetailKeranjang");
-=======
 const Produk = require("./Produk");
->>>>>>> 6a72b868ebda66a19d4373ef1b340970d2d1387f
 
 const Keranjang = sequelize.define(
   "keranjang",
@@ -15,21 +11,6 @@ const Keranjang = sequelize.define(
       autoIncrement: true
     },
 
-<<<<<<< HEAD
-    total: {
-      type: Sequelize.INTEGER
-    }
-  },
-  {
-    timestamps: false,
-    freezeTableName: true,
-    tableName: "keranjang"
-  }
-);
-
-Keranjang.hasMany(DetailKeranjang, { foreignKey: "id_keranjang" });
-
-=======
     id_produk: {
       type: Sequelize.INTEGER
     },
@@ -46,5 +27,4 @@ Keranjang.hasMany(DetailKeranjang, { foreignKey: "id_keranjang" });
 );
 
 Keranjang.belongsTo(Produk, { foreignKey: "id_produk" });
->>>>>>> 6a72b868ebda66a19d4373ef1b340970d2d1387f
 module.exports = Keranjang;
