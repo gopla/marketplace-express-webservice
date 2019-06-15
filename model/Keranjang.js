a8fe3625663bb47d7d0ab59b8b04a50639646c1c
Naufal Yudhistira <yudhistn@gmail.com>
2019-06-15 13:39:42 +0700

    Keranjang

--- model/Keranjang.js
+++ model/Keranjang.js
@@ -1,25 +1,27 @@
-const Sequelize = require('sequelize')
-const sequelize = require('../config/db')
-const DetailKeranjang = require('./DetailKeranjang')
+const Sequelize = require("sequelize");
+const sequelize = require("../config/db");
+const DetailKeranjang = require("./DetailKeranjang");
 
 const Keranjang = sequelize.define(
-    "keranjang", {
-        id_keranjang: {
-            type: Sequelize.INTEGER,
-            primaryKey: true,
-            autoIncrement:true
-        },
+  "keranjang",
+  {
+    id_keranjang: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
 
-        total: {
-            type: Sequelize.INTEGER,
-        }
-    },{
-        timestamps: false,
-        freezeTableName: true,
-        tableName: 'keranjang'
+    total: {
+      type: Sequelize.INTEGER
     }
-)
+  },
+  {
+    timestamps: false,
+    freezeTableName: true,
+    tableName: "keranjang"
+  }
+);
 
-Keranjang.hasMany(DetailKeranjang, {foreignKey: 'id_keranjang'})
+Keranjang.hasMany(DetailKeranjang, { foreignKey: "id_keranjang" });
 
-module.exports = Keranjang
+module.exports = Keranjang;
