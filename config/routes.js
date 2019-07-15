8f7717812a90efb59bc3fe3f336fcc53ca71accc
daffadwiputra <daffaputradamar@gmail.com>
2019-07-15 16:11:45 +0700

    add produk docs

---
+++ config/routes.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+module.exports = router
+
+router.use("/produk", require("../routes/produkRouter"));
+router.use("/keranjang", require("../routes/keranjangRouter"));
+router.use("/transaksi", require("../routes/transaksiRouter"));
+router.use("/provinsi", require("../routes/provinsiRouter"));
+router.use("/ongkir", require("../routes/ongkirRouter"));
