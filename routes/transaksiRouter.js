5b1529630dd2fb4ce417585bafc53034ad214768
Daffa Akbar <daffaputradamar@gmail.com>
2019-10-04 01:18:20 +0700

    adjusting new transactional model

--- routes/transaksiRouter.js
+++ routes/transaksiRouter.js
@@ -9,6 +9,8 @@ const upload = multer()
 const verifyToken = require('../middlewares/verifyToken')
 
 router.use(verifyToken)
+router.put('/:id/konfirmasi', transaksiController.confirm)
+router.get('/unconfirmed', transaksiController.indexUnconfirmed)
 router.get('/', transaksiController.index)
 router.get('/:id/detail', transaksiController.indexDetail)
 router.get('/:id', transaksiController.show)
@@ -19,7 +21,5 @@ router.put('/:id', upload.single('bukti_bayar'), transaksiController.update)
 router.put('/:id/detail/:id_detail', transaksiController.updateDetail)
 router.delete('/:id', transaksiController.delete)
 router.delete('/:id/detail/:id_detail', transaksiController.deleteDetail)
-router.put('/:id/konfirmasi', transaksiController.confirm)
-router.get('/unconfirmed', transaksiController.indexUnconfirmed)
 
 module.exports = router
